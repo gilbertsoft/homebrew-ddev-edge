256d2dc988bd4bcb5b6ea5989b5566042b7df6c4
rfay <randy@randyfay.com>
2022-03-11 02:25:34 +0000

    Homebrew bump to v1.19.0

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -1,8 +1,8 @@
 class Ddev < Formula
   desc "Local web development system"
   homepage "https://ddev.readthedocs.io/"
-  url "https://github.com/drud/ddev/archive/v1.19.0-rc4.tar.gz"
-  sha256 "53c1b2a1323ee4d0ddf1edd3d5b8dd20a2ead5c3bbb5e5acc7d0d0af43665b23"
+  url "https://github.com/drud/ddev/archive/v1.19.0.tar.gz"
+  sha256 "19c76378405563c4989eade93de367ff0991bf16a4b15efb0b82ff5bb03792a6"
   license "apache-2.0"
   head "https://github.com/drud/ddev.git", branch: "master"
 
@@ -12,10 +12,10 @@ class Ddev < Formula
   depends_on "make" => :build
 
   bottle do
-    root_url "https://github.com/drud/ddev/releases/download/v1.19.0-rc4/"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bded18158dc19ddce2b807f41dab51d3f35f5a99e03b722fe7583ccb3717672d"
-    sha256 cellar: :any_skip_relocation, high_sierra: "e39ecb1054df11d20ea708467959cb06d74fc6ac047494227be459b99b4368c1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8ed6069d68ff4e256bd8aff1375db4b3f6e1c4c93b319a3ebc0caa85ceb4989b"
+    root_url "https://github.com/drud/ddev/releases/download/v1.19.0/"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2ed2451b88c111cc1569f81490e20fdd0a176d305a32136c3dfcab610b7ce6c5"
+    sha256 cellar: :any_skip_relocation, high_sierra: "658d3049940678b67a47fe4b59d13d64e911e3aa5cf50207570d374799aaeefe"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e409da4c02455d02eae789bc88c35b29f736021ab1d01b9595d984526bce54d3"
   end
   def install
     system "make", "VERSION=v#{version}", "COMMIT=v#{version}"
